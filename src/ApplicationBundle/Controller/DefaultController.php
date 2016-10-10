072134e12b3f972dee446058401505508aa9a73b
morganessimplon <morganessimplon@gmail.com>
2016-10-10 14:55:41 +0200

    Modif Css et Login

---
+++ src/ApplicationBundle/Controller/DefaultController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 10/10/2016
+ * Time: 1:19 PM
+ */
---
+++ src/FormationBundle/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        $mail = $this->getUser()->getEmail();
+        $test = $this->getDoctrine()->getManager()->getRepository('AppBundle:Applicant')->findOneBy(array('mail' => $mail));
+        if($test) {
+            $user = $this->getDoctrine()->getManager()->getRepository('AppBundle:Applicant')->findOneBy(array('mail' => $mail));
+        } else{
+            $user = $this->getUser();
+        }
+        return $this->render('AppBundle:Default:index.html.twig', array('user' => $user));
+    }
+}
